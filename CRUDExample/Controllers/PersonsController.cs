582a8e013d7e22a3659c38b094457de953dd2dbe
Liang Wu <liangwuweb@gmail.com>
2023-05-14 11:22:20 -0500

    route attribute

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -5,17 +5,21 @@ using ServiceContracrs.Enums;
 
 namespace CRUDExample.Controllers
 {
+    [Route("persons")]
     public class PersonsController : Controller
     {
         //private fields
         private readonly IPersonService _personService;
+        private readonly ICountriesService _countriesService;
 
-        public PersonsController(IPersonService personService)
+        public PersonsController(IPersonService personService, ICountriesService countriesService)
         {
             _personService = personService;
+            _countriesService = countriesService;
         }
 
-        [Route("persons/index")]
+        // url /preson/index
+        [Route("index")]
         [Route("/")]
         public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrder sortOrder = SortOrder.ASC)
         {
@@ -34,10 +38,43 @@ namespace CRUDExample.Controllers
             ViewBag.CurrentSearchString = searchString;
 
             //Sorting
-            List<PersonResponse>  sortedPersons = _personService.GetSortedPerson(persons, sortBy, sortOrder);
+            List<PersonResponse> sortedPersons = _personService.GetSortedPerson(persons, sortBy, sortOrder);
             ViewBag.CurrentSortBy = sortBy;
             ViewBag.CurrentSortOrder = sortOrder;
             return View(sortedPersons);
         }
+
+        // url /preson/create
+        [Route("create")]
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            List<CountryResponse> countires = _countriesService.GetAllCountries();
+            ViewBag.Countries = countires;
+            return View();
+        }
+
+        // url /preson/create
+        [Route("create")]
+        [HttpPost]
+        public IActionResult Create(PersonAddRequest personAddRequest)
+        {
+
+            if (!ModelState.IsValid) 
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries;
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View();
+
+            }
+
+            PersonResponse personResponse = _personService.AddPerson(personAddRequest);
+            
+            //redirect to Index() action method, and makes another get request to perosns/index
+            return RedirectToAction("Index", "Persons");
+        }
+
+
     }
 }
